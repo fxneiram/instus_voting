1ca976cb8017ff48c47b0fa5e54405a3c3dce51f
Faver Neira <fxneiram@gmail.com>
2021-10-25 06:42:48 -0500

    Eliminacion de dato de pueba en seeder

--- database/seeders/VotingsTableSeeder.php
+++ database/seeders/VotingsTableSeeder.php
@@ -15,30 +15,6 @@ class VotingsTableSeeder extends Seeder
      */
     public function run()
     {
-        $votacion = Voting::create([
-            'id' => 1,
-            'name' => 'votación 1',
-            'description' => 'bla bnla',
-            'begin_at' => '2021-24-10',
-            'end_at' => '2021-25-10',
-        ]);
 
-        $op1 = Option::create([
-            'voting_id' => 1,
-            'description' => 'op 1',
-        ]);
-
-        $op1 = Option::create([
-            'voting_id' => 1,
-            'description' => 'op 2',
-        ]);
-
-        $op1 = Option::create([
-            'voting_id' => 1,
-            'description' => 'op 3',
-        ]);
-        //
-
-        Voting::factory()->count(20)->create();
     }
 }
