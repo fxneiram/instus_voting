4a4bee4b8d846cc9782357a77a8f2b62c3fb3ce4
Faver Neira <fxneiram@gmail.com>
2021-10-25 06:18:37 -0500

    Corrección menu según permisos

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,45 +1,49 @@
 @can('autogestion')
-<li class="nav-item">
-    <a href="{{url('/home') }}"
-       class="nav-link {{ Request::is('home*') ? 'active' : '' }}">
-        <i class="nav-icon fa fa-home"></i>
-        <p>Inicio</p>
-    </a>
-</li>
+    <li class="nav-item">
+        <a href="{{url('/home') }}"
+           class="nav-link {{ Request::is('home*') ? 'active' : '' }}">
+            <i class="nav-icon fa fa-home"></i>
+            <p>Inicio</p>
+        </a>
+    </li>
 @endcan
 
-<li class="nav-item">
-    <a href="{{ route('users.index') }}"
-       class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
-        <i class="nav-icon fa fa-user"></i>
-        <p>Usuario</p>
-    </a>
-</li>
+@can('gestion usuarios')
+    <li class="nav-item">
+        <a href="{{ route('users.index') }}"
+           class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
+            <i class="nav-icon fa fa-user"></i>
+            <p>Usuario</p>
+        </a>
+    </li>
+@endcan
 
+@can('gestion permisos')
+    <li class="nav-item">
+        <a href="{{ route('permissions.index') }}"
+           class="nav-link {{ Request::is('permissions*') ? 'active' : '' }}">
+            <i class="nav-icon fa fa-check"></i>
+            <p>Permisos</p>
+        </a>
+    </li>
+@endcan
 
-<li class="nav-item">
-    <a href="{{ route('permissions.index') }}"
-       class="nav-link {{ Request::is('permissions*') ? 'active' : '' }}">
-        <i class="nav-icon fa fa-check"></i>
-        <p>Permisos</p>
-    </a>
-</li>
+@can('gestion roles')
+    <li class="nav-item">
+        <a href="{{ route('roles.index') }}"
+           class="nav-link {{ Request::is('roles*') ? 'active' : '' }}">
+            <i class="nav-icon fa fa-users"></i>
+            <p>Roles</p>
+        </a>
+    </li>
+@endcan
 
-
-<li class="nav-item">
-    <a href="{{ route('roles.index') }}"
-       class="nav-link {{ Request::is('roles*') ? 'active' : '' }}">
-        <i class="nav-icon fa fa-users"></i>
-        <p>Roles</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('votings.index') }}"
-       class="nav-link {{ Request::is('votings*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-chart-pie"></i>
-        <p>Votaciones</p>
-    </a>
-</li>
-
+@can('gestion votacion')
+    <li class="nav-item">
+        <a href="{{ route('votings.index') }}"
+           class="nav-link {{ Request::is('votings*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-chart-pie"></i>
+            <p>Votaciones</p>
+        </a>
+    </li>
+@endcan
